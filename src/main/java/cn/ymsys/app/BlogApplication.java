32d56251a21b5d5e65b0d0a00b65520226a6c4b1
mjy <296629801@qq.com>
2019-06-21 18:32:07 +0800

    1

--- src/main/java/cn/ymsys/app/BlogApplication.java
+++ src/main/java/cn/ymsys/app/BlogApplication.java
@@ -1,6 +1,5 @@
 package cn.ymsys.app;
 
-import com.terran4j.commons.api2doc.config.EnableApi2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @ComponentScan("cn.ymsys.api.*")
 @SpringBootApplication
 @EnableAsync
-@EnableApi2Doc
 public class BlogApplication {
 
     public static void main(String[] args) {
